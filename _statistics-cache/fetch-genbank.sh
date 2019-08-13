5e9689a3b20f1b3fe2a79f527eceb9535bad2e9b
Brian P. Walenz <thebri@gmail.com>
2019-08-13 08:42:46 -0400

    Add some utilities/data for accessing genbank assemblies.

---
+++ _statistics-cache/fetch-genbank.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ -z $NCBI_API_KEY ] ; then
+  echo NCBI_API_KEY not set.
+  exit
+fi
+
+if [ ! -e genbank.xml ] ; then
+  echo Fetching genbank.xml.
+
+  esearch -db bioproject -q 'PRJNA489243' \
+  | \
+  elink -db bioproject -target assembly -name bioproject_assembly_all \
+  | \
+  esummary \
+  > genbank.xml
+fi
+
+if [ ! -e genbank.xml.map ] ; then
+  echo Parsing genbank.xml.
+
+  #  Extract elements 'Genbank', 'AssemblyName' and 'AssemblyType' from all
+  #  'DocumentSummary' elements IF it contains a `Synonym` element (which is
+  #  where the 'Genbank' element is).
+
+  xtract \
+    -input genbank.xml \
+    -pattern DocumentSummary -if Synonym -element Genbank AssemblyName AssemblyType \
+  > genbank.map
+fi
+
+exit 0
